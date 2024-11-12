49c27237485cfe1b8cedbe93d6032c47ec59cc15
soomin <s2305@e-mirim.hs.kr>
2024-11-12 15:14:36 +0900

    [feat] choice 추가

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import './App.css';
 import Home from "./home/Home";
 import Voice from "./voice/Voice";
+import Choice from "./choice/Choice";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/voice" element={<Voice />} />
+        <Route path="/" element={<Choice />} />
       </Routes>
     </Router>
   );
