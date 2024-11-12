648ed0fefce4f62abc8ddeb5e7669e28b3cac780
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-12 15:56:08 +0900

    [fix] 깃허브 에러 이슈...

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 import Home from "./home/Home";
-import Voice from "./voice/Voice";
 import Choice from "./choice/Choice";
 import Order from "./order/Order";
 import Address from "./address/Address";
@@ -14,7 +13,6 @@ function App() {
     <Router>
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/voice" element={<Voice />} />
         <Route path="/choice" element={<Choice />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/" replace />} />  {/* 잘못된 경로일 경우 홈으로 리다이렉트 */}
