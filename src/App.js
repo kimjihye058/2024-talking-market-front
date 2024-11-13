bf40310a4dfe26e19ba84e5c007c2277f33b44ac
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-13 18:29:44 +0900

    [feat] ChoiceList, MartMap js와 css 파일 추가

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 import Home from "./home/Home";
-import Choice from "./choice/Choice";
 import Order from "./order/Order";
+import Choice from "./choice/Choice";
+import ChoiceList from "./choice_list/ChoiceLIst";
 import Address from "./address/Address";
 import AddressCheck from "./address_check/AddressCheck";
+import MartMap from "./mart_map/MartMap";
 import Success from "./success/Success";
 
 function App() {
@@ -13,12 +15,14 @@ function App() {
     <Router>
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/choice" element={<Choice />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/" replace />} />  {/* 잘못된 경로일 경우 홈으로 리다이렉트 */}
         <Route path="/order" element={<Order />} />
+        <Route path="/choice" element={<Choice />} />
+        <Route path="/choicelist" element={<ChoiceList />} />
         <Route path="/address" element={<Address />} />
         <Route path="/addresscheck" element={<AddressCheck />} />
+        <Route path="/martmap" element={<MartMap />} />
         <Route path="/success" element={<Success />} />
       </Routes>
     </Router>
