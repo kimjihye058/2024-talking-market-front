2d428c7945c95efb0dbd04ef16dd9321d3070686
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-13 16:56:51 +0900

    [feat] AddressCheck.css완성

--- src/address_check/AddressCheck.js
+++ src/address_check/AddressCheck.js
@@ -8,18 +8,18 @@ const AddressCheck = () => {
             <header className="header2">
                 <span className="logo2">주소입력</span>
             </header>
-            <div>
-                <p>수락 리버시티 1단지 102동 501호</p>
+            <div className="address">
+                <p>수락 리버시티 1단지<br/> 102동 501호</p>
             </div>
-            <div>
+            <div className="checkaddress">
                 <p>이 주소가 맞나요?</p>
             </div>
-            <div>
-                <div>
+            <div className="button">
+                <div className="noBtn">
                     <img src="/images/noBtn.png"></img>
                     <p>아니요</p>
                 </div>
-                <div>
+                <div className="yesBtn">
                     <img src="/images/yesBtn.png"></img>
                     <p>네</p>
                 </div>
