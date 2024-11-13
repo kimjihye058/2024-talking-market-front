2cd851a4f557a1d61bd3957508a2c03fdc7ec12c
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-13 18:25:04 +0900

    [feat] Select.css margin 수정

--- src/choice/Select.js
+++ src/choice/Select.js
@@ -9,7 +9,9 @@ const Select = () => {
                 <p id="select_product">다우니 프리미엄 세탁 세제 액체형 실내건조 2.8L</p>
                 <p id="select_count">수량 :  개</p>
                 <p id="select_price">가격 : 원</p>
-                <div className="select_btn_div">
+            </div>
+            <div className="select_btn_div1">
+                <div className="select_btn_div2">
                     <button id="again_btn">다시 입력하기</button>
                     <button id="add_btn">추가 주문하기</button>
                     <button id="order_btn">주문하기</button>
