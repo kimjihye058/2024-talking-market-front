0db4687dfdc122cd83a9ef902f905efc101ca411
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-15 07:49:19 +0900

    [feat] choicelist.css 완성

--- src/choice_list/ChoiceLIst.js
+++ src/choice_list/ChoiceLIst.js
@@ -16,8 +16,24 @@ const ChoiceList = () => {
                 <p>가격 : 원</p>
                 <p>총 수량 : 개</p>
             </div>
-            <div className="products">
-
+            <div className="products1">
+                <img 
+                    id="product_img"
+                    src="https://sitem.ssgcdn.com/49/94/56/item/1000051569449_i1_750.jpg" 
+                    alt="다우니 프리미엄 세탁 세제"
+                />
+                <div className="products2">
+                    <p id="product_name">다우니 프리미엄 세탁 세제 액체형 실내건조 2.8L</p>
+                    <p id="product_price">가격 : 44000원</p>
+                    <div className="product_div3">
+                        <select id="product_count" name="options">
+                            {[...Array(8).keys()].map(i => (
+                                <option key={i + 1} value={i + 1}>{i + 1}</option>
+                            ))}
+                        </select>
+                        <span id="count_text">개</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
